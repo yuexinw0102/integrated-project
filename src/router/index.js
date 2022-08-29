0c670433bca77ae9c646d718aac724eefef676fc
yuexinw <yuexinw097@163.com>
2022-08-29 20:40:02 +0800

    登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect:"/login"
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/home',
+    name: 'home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomeView.vue')
+  },
+  
 ]
 
 const router = new VueRouter({
---
+++ src/views/LoginView.vue
@@ -0,0 +1,73 @@
+<template>
+	<div id="root">
+		<el-row type="flex" justify="center" align="middle" id="row">
+			<el-card class="el-card" shadow="never" :body-style="{ 
+				'text-align': 'center' ,
+				'background-color': '#00A0C2' ,
+			}">
+				<template slot#header>
+					<h1>生鲜电商-后台系统</h1>
+				</template>
+				<el-form :model="form">
+					<el-form-item>
+						<el-input v-model="form.account" placeholder="账号" />
+					</el-form-item>
+					<el-form-item>
+						<el-input type="password" v-model="form.password" placeholder="密码" />
+					</el-form-item>
+					<el-form-item>
+						<el-button style="width: 100%" type="primary" @click="handler_login" :loading="isLoading">登录
+						</el-button>
+					</el-form-item>
+				</el-form>
+			</el-card>
+		</el-row>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Login",
+		data() {
+			return {
+				form: {
+					account: "",
+					password: "",
+				},
+				isLoading: false,
+			};
+		},
+		methods: {
+			handler_login() {
+				console.log("登录");
+				this.isLoading = true;
+				setTimeout(() => {
+					this.isLoading = false;
+				}, 2000);
+				this.$router.replace({
+					path: "/home",
+				});
+			},
+		},
+	};
+</script>
+
+<style lang="less" scoped>
+	#root {
+		background-image: url("../assets/login.jpg");
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
+		height: 100%;
+		width: 100%;
+		overflow: hidden;
+	}
+
+	.el-card {
+		border-color: #00A0C2;
+	}
+
+	#row {
+		height: 300px;
+		margin-top: 300px;
+	}
+</style>
