184902ef5012ce369e535d4b74935d31bfe60ec9
Liiiii3 <976292163@qq.com>
2022-09-06 15:18:41 +0800

    分页组件

---
+++ src/components/Pagination.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="block">
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, prev, pager, next, sizes, jumper"
+      :total="400"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  props: {
+    },
+    data() {
+      return {
+        currentPage1: 5,
+        currentPage2: 5,
+        currentPage3: 5,
+        currentPage4: 4,
+      };
+    },
+    methods: {
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      },
+    },
+  };
+</script>
+
+<style></style>
