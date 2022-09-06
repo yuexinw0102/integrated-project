94fd18ea23e390fac441ef9b6e84087bdc952abf
yuexinw <yuexinw097@163.com>
2022-09-06 14:42:46 +0800

    mock引入

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 import store from './store'
 import '@/elementui'
 
+require('./mock/warehouse_mock') //文档引入
+
 // import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
 // Vue.use(ElementUI);
