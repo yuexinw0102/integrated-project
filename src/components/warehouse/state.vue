0ab8c2bc3e9ea2f6fadc35d11351a2fa8c316c57
yuexinw <yuexinw097@163.com>
2022-09-22 11:03:48 +0800

    仓库详情页面功能实现

--- src/components/warehouse/state.vue
+++ src/components/warehouse/state.vue
@@ -8,8 +8,7 @@
         <el-form-item label="仓库名称" prop="name">
           <el-input v-model="form.name" placeholder="仓库名称"></el-input>
         </el-form-item>
-
-        <el-form-item v-if="this.dataId == ''">
+        <el-form-item v-if="this.dataId == ''" label="所在城市">
           <v-distpicker @selected="onSelected"></v-distpicker>
         </el-form-item>
         <el-row v-else type="flex" justify="">
@@ -69,15 +68,40 @@
         <el-form-item label="关联小区" type="flex" justify="">
           <el-input
             placeholder="请输入小区名称"
-            v-model="search.searchCommunity"
+            v-model="searchData.searchCommunity"
             style="width: 200px"
           ></el-input>
           <el-button type="primary" @click="search('community')"
             >查询</el-button
           >
-          <el-button type="primary" @click="increase()">新增小区</el-button>
+          <el-button type="primary" @click="dialogFormVisible = true"
+            >新增小区</el-button
+          >
+
+          <el-dialog title="新增小区" :visible.sync="dialogFormVisible">
+            <el-form :model="increaseData" ref="increaseData">
+              <el-form-item label="请输入名称" prop="name">
+                <el-input
+                  v-model="increaseData.name"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+              <el-form-item label="请输入地址" prop="value">
+                <el-input
+                  v-model="increaseData.value"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="increase('community','increaseData')"
+                >确 定</el-button
+              >
+            </div>
+          </el-dialog>
           <el-table
-            :data="search.communityData"
+            :data="searchData.communityData"
             style="width: 100%"
             border
             size="small"
@@ -102,13 +126,37 @@
         <el-form-item label="配置骑手" type="flex" justify="">
           <el-input
             placeholder="请输入骑手名称"
-            v-model="search.searchRider"
+            v-model="searchData.searchRider"
             style="width: 200px"
           ></el-input>
           <el-button type="primary" @click="search('rider')">查询</el-button>
-          <el-button type="primary">新增骑手</el-button>
+          <el-button type="primary" @click="dialogFormVisible1 = true"
+            >新增骑手</el-button
+          >
+          <el-dialog title="新增骑手" :visible.sync="dialogFormVisible1">
+            <el-form :model="increaseData" ref="increaseData">
+              <el-form-item label="请输入姓名">
+                <el-input
+                  v-model="increaseData.name"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+              <el-form-item label="请输入电话">
+                <el-input
+                  v-model="increaseData.value"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="increase('rider','increaseData')"
+                >确 定</el-button
+              >
+            </div>
+          </el-dialog>
           <el-table
-            :data="search.riderData"
+            :data="searchData.riderData"
             style="width: 100%"
             :row-class-name="state"
             border
@@ -142,13 +190,37 @@
         <el-form-item label="配置分拣员" type="flex" justify="">
           <el-input
             placeholder="请输入分拣员名称"
-            v-model="search.searchSorting"
+            v-model="searchData.searchSorting"
             style="width: 200px"
           ></el-input>
           <el-button type="primary" @click="search('sorting')">查询</el-button>
-          <el-button type="primary">新增分拣员</el-button>
+          <el-button type="primary" @click="dialogFormVisible2 = true"
+            >新增分拣员</el-button
+          >
+          <el-dialog title="新增分拣员" :visible.sync="dialogFormVisible2">
+            <el-form :model="increaseData" ref="increaseData">
+              <el-form-item label="请输入姓名" prop="name">
+                <el-input
+                  v-model="increaseData.name"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+              <el-form-item label="请输入电话" prop="value">
+                <el-input
+                  v-model="increaseData.value"
+                  autocomplete="off"
+                ></el-input>
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="increase('sorting','increaseData')"
+                >确 定</el-button
+              >
+            </div>
+          </el-dialog>
           <el-table
-            :data="search.sortingData"
+            :data="searchData.sortingData"
             style="width: 100%"
             border
             size="small"
@@ -217,7 +289,7 @@ export default {
       showData: JSON.parse(localStorage.getItem("warehouseData")),
       area: "",
       city: "",
-      search: {
+      searchData: {
         searchCommunity: "",
         searchRider: "",
         searchSorting: "",
@@ -240,6 +312,13 @@ export default {
         city: "",
         area: "",
       },
+      increaseData: {
+        name: "",
+        value: "",
+      },
+      dialogFormVisible: false,
+      dialogFormVisible1: false,
+      dialogFormVisible2: false,
     };
   },
   computed: {
@@ -286,7 +365,23 @@ export default {
     deleteCommunity(index) {
       this.form.community.splice(index, 1);
     },
-
+    increase(dataName,formName) {
+      if (dataName == "community") {
+        this.dialogFormVisible = false;
+        this.form.community.push({name:this.increaseData.name,address:this.increaseData.value});
+      } else if (dataName == "rider") {
+        this.dialogFormVisible1 = false;
+        this.riderData = this.form[dataName].filter(
+          (item) => item.name == this.searchRider
+        );
+      } else if (dataName == "sorting") {
+        this.dialogFormVisible2 = false;
+        this.sortingData = this.form[dataName].filter(
+          (item) => item.name == this.searchSorting
+        );
+      }
+      this.$refs[formName].resetFields();
+    },
     onSelected(data) {
       const { province, city, area } = data;
       if (!province.code && !city.code && !city.code) return;
@@ -355,9 +450,9 @@ export default {
         }
       }
     });
-    this.search.communityData = this.form.community;
-    this.search.riderData = this.form.rider;
-    this.search.sortingData = this.form.sorting;
+    this.searchData.communityData = this.form.community;
+    this.searchData.riderData = this.form.rider;
+    this.searchData.sortingData = this.form.sorting;
   },
   components: {
     VDistpicker,
